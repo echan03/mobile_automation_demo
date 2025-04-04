ba69a600312f2daf3eae0eb8a4baee0706b2c08d
ktrechan <ktr_echan@sorenson.com>
2025-04-04 14:10:13 +0800

    added tags to tests

--- calculatorTests/calculate_test.py
+++ calculatorTests/calculate_test.py
@@ -1,22 +1,27 @@
+import pytest
 from calculatorTests.basetest import BaseTest
 
 class Test_Calculate(BaseTest):
     
+    @pytest.mark.smoke
     def test_add(self):
         self.initialize()
         self.calculatorPage.add("1", "1")
         assert self.calculatorPage.getResult().__contains__("2")
         
+    @pytest.mark.smoke
     def test_subtract(self):
         self.initialize()
         self.calculatorPage.subtract("5", "1")
         assert self.calculatorPage.getResult().__contains__("4")
         
+    @pytest.mark.smoke
     def test_multiply(self):
         self.initialize()
         self.calculatorPage.multiply("5", "5")
         assert self.calculatorPage.getResult().__contains__("25")
         
+    @pytest.mark.smoke
     def test_divide(self):
         self.initialize()
         self.calculatorPage.divide("10", "2")
